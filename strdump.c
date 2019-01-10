372df78e036f319242897ea897649258eb9d4136
JunYu Lai <chaojisuxiaowoniu@163.com>
2019-01-10 23:37:14 +0800

    补充说明

--- strdump.c
+++ strdump.c
@@ -4,6 +4,8 @@
 #include <limits.h>
 
 /*---函数：用十六进制和二进制数显示字符串内部的字符---*/
+/*明解C语言的作者为了在显示字符时，不受1个字节的位数的影响，使用(CHAR_BIT + 3) / 4 位表示十六进制，使用CHAR_BIT位表示
+二进制。CHAR_BIT一般定义为8*/
 void strdump(const char *s)
 {
     
