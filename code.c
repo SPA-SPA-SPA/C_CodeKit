7c4814bc7adba40d23e437f3bb8e5726b2dafde9
JunYu Lai <chaojisuxiaowoniu@163.com>
2019-01-10 15:47:21 +0800

    code

---
+++ code.c
@@ -0,0 +1,41 @@
+/*显示字符和字符编码*/
+#include <ctype.h>
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    for(i = 0; i <= CHAR_MAX; i++)
+    {
+        switch (i)
+        {
+            case '\a':
+                printf("\\a");
+                break;
+            case '\b':
+                printf("\\b");
+                break;
+            case '\f':
+                printf("\\f");
+                break;
+            case '\n':
+                printf("\\n");
+                break;
+            case '\r':
+                printf("\\r");
+                break;
+            case '\t':
+                printf("\\t");
+                break;
+            case '\v':
+                printf("\\v");
+                break;
+            default:
+                printf("%c", isprint(i) ? i : ' ');
+                break;
+        }
+        printf("%02x\n", i);
+    }
+    return 0;
+}
